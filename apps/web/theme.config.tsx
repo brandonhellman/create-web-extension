ef468bcbe623ea097ec59f4ab536fa99f88e8572
Brandon Hellman <me@brandonhellman.com>
2024-11-23 13:55:04 -0800

    Update theme.config.tsx

--- apps/web/theme.config.tsx
+++ apps/web/theme.config.tsx
@@ -6,8 +6,25 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/brandonhellman/browser-ext',
   },
   docsRepositoryBase: 'https://github.com/brandonhellman/browser-ext/tree/main/apps/web',
+  head: (
+    <>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      />
+      <meta
+        property="og:title"
+        content="@browser-ext"
+      />
+      <meta
+        property="og:description"
+        content="The next generation of browser extension development"
+      />
+      <title>@browser-ext</title>
+    </>
+  ),
   footer: {
-    content: 'Nextra Docs Template',
+    content: '@browser-ext',
   },
 };
 
