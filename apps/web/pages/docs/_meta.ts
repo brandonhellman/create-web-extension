1a1da2f72052e15a08efb73e86375b8903982a2d
Brandon Hellman <me@brandonhellman.com>
2024-11-29 21:18:45 -0800

    Update _meta.ts

--- apps/web/pages/docs/_meta.ts
+++ apps/web/pages/docs/_meta.ts
@@ -2,5 +2,5 @@ export default {
   introduction: 'Introduction',
   'getting-started': 'Getting Started',
   utils: 'Utility Functions',
-  'webpack-configuration': 'Webpack Configuration'
+  'webpack-configuration': 'Webpack Configuration',
 };
