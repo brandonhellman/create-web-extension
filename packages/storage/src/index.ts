df5c432cb1e09bbb6e45dc983fddbd7638386360
Brandon Hellman <me@brandonhellman.com>
2024-12-02 12:36:34 -0800

    Update index.ts

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -1,12 +1,18 @@
-export async function get<T>(key: string): Promise<T | undefined> {
+async function get<T>(key: string): Promise<T | undefined> {
   const result = await chrome.storage.local.get(key);
   return result[key];
 }
 
-export async function set<T>(key: string, value: T) {
+async function set<T>(key: string, value: T) {
   await chrome.storage.local.set({ [key]: value });
 }
 
-export async function remove(key: string) {
+async function remove(key: string) {
   await chrome.storage.local.remove(key);
 }
+
+export const storage = {
+  get,
+  set,
+  remove,
+};
