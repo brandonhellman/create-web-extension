06a14038b157b9f388fdef11eeebb42b6f7aae6c
Brandon Hellman <me@brandonhellman.com>
2024-12-01 22:52:44 -0800

    added base storage functions

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -1,4 +1,12 @@
-export function example() {
-  console.log('example');
-  return 'example';
+export async function get<T>(key: string): Promise<T | undefined> {
+  const result = await chrome.storage.local.get(key);
+  return result[key];
+}
+
+export async function set<T>(key: string, value: T) {
+  await chrome.storage.local.set({ [key]: value });
+}
+
+export async function remove(key: string) {
+  await chrome.storage.local.remove(key);
 }
