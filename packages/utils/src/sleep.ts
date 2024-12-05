3a04c3dc12cc9120d4b604e9f2f2043296715a48
Brandon Hellman <me@brandonhellman.com>
2024-12-04 22:54:03 -0800

    Update sleep.ts

--- packages/utils/src/sleep.ts
+++ packages/utils/src/sleep.ts
@@ -1,3 +1,9 @@
-export async function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+/**
+ * Sleep for a specified amount of time
+ * @param minMs - Minimum milliseconds to sleep
+ * @param maxMs - Optional maximum milliseconds to sleep. If provided, will sleep for a random duration between ms and maxMs
+ */
+export async function sleep(minMs: number, maxMs?: number) {
+  const duration = maxMs ? Math.floor(Math.random() * (maxMs - minMs + 1)) + minMs : minMs;
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
