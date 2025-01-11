0ce73c6442b7c085923eea11915fbf0048a379cc
Brandon Hellman <me@brandonhellman.com>
2025-01-10 23:21:22 -0800

    added with-all

---
+++ examples/with-all/src/contents/index.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+console.log('contents/index.ts');
+
+function App() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [count, setCount] = useState(0);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="absolute inset-0 z-50 flex flex-col items-center justify-center bg-black bg-opacity-50">
+      <div className="rounded bg-white p-4">
+        <p className="text-gray-500">Count: {count}</p>
+        <div className="flex flx-row gap-2">
+          <button
+            className="mt-4 rounded bg-blue-500 px-4 py-2 text-white"
+            onClick={() => setCount((prev) => prev + 1)}
+          >
+            Increment
+          </button>
+          <button
+            className="mt-4 rounded bg-red-500 px-4 py-2 text-white"
+            onClick={() => setIsOpen(false)}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+const root = createRoot(container);
+root.render(<App />);
