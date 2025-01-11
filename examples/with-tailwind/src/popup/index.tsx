4e0a0760886b295005eaef9494b7ee956c914d6f
Brandon Hellman <me@brandonhellman.com>
2025-01-10 20:42:25 -0800

    with-tailwind wip

---
+++ examples/with-tailwind/src/popup/index.tsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+import { createRoot } from 'react-dom/client';
+
+console.log('popup/index.tsx');
+
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <div className="flex h-96 w-96 flex-col items-center justify-center">
+      <div className="rounded bg-white p-4 shadow">Hello from Popup!</div>
+    </div>,
+  );
+}
