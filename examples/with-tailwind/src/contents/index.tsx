fb1c283ac47ae7df70120b339c6a7179203e912f
Brandon Hellman <me@brandonhellman.com>
2025-01-10 22:54:09 -0800

    Update index.tsx

--- examples/with-tailwind/src/contents/index.tsx
+++ examples/with-tailwind/src/contents/index.tsx
@@ -12,21 +12,24 @@ function App() {
   }
 
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold">Hello, Tailwind!</h1>
-      <p className="text-gray-500">Count: {count}</p>
-      <button
-        className="mt-4 rounded bg-blue-500 px-4 py-2 text-white"
-        onClick={() => setCount((prev) => prev + 1)}
-      >
-        Increment
-      </button>
-      <button
-        className="mt-4 rounded bg-red-500 px-4 py-2 text-white"
-        onClick={() => setIsOpen(false)}
-      >
-        Remove
-      </button>
+    <div className="absolute inset-0 z-50 flex flex-col items-center justify-center bg-black bg-opacity-50">
+      <div className="rounded bg-white p-4">
+        <p className="text-gray-500">Count: {count}</p>
+        <div className="flex flx-row gap-2">
+          <button
+            className="mt-4 rounded bg-blue-500 px-4 py-2 text-white"
+            onClick={() => setCount((prev) => prev + 1)}
+          >
+            Increment
+          </button>
+          <button
+            className="mt-4 rounded bg-red-500 px-4 py-2 text-white"
+            onClick={() => setIsOpen(false)}
+          >
+            Remove
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
